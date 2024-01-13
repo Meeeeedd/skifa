d68b1675d439fa6c36203870a8f5293091810821
Ferid HELALI <helaliferid@gmail.com>
2024-01-13 10:03:16 +0100

    add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,10 +2,23 @@ import { Button } from '@chakra-ui/react'
 
 import './App.css'
 
+function Person(props){
+  return(
+    <>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+    </>
+  )
+}
+
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <Button colorScheme='blue'>Button</Button>
+=======
+      <Person name="John" age="30"/>
+>>>>>>> f8121d9 (add)
     </>
   )
 }
